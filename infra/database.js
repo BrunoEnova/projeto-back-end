842c56d6845cf3235d73aa7796bda54feaf8c7ac
BrunoEnova <bruno.amaral13@enova.educacao.ba.gov.br>
2025-09-17 18:26:00 +0000

    feat:criando função query do banco de dados

---
+++ infra/database.js
@@ -0,0 +1,28 @@
+const { Client } = require("pg");
+
+function query(sql) {
+  const client = new Client({
+    host: "",
+    port: 5432,
+    user: "",
+    password: "",
+  });
+}
+
+const { Client } = require("pg");
+
+async function query(sql, args = []) {
+  const client = new Client({
+    host: "localhost",
+    port: 5432,
+    user: "",
+    password: "",
+  });
+  await client.connect();
+
+  const res = await client.query(sql, args);
+
+  await client.end();
+  return res.rows;
+}
+database: "";
