5601ab12db21c255a0f6a998c219473a0d976e70
BrunoEnova <bruno.amaral13@enova.educacao.ba.gov.br>
2025-11-05 16:41:34 +0000

    http server inicial

--- infra/repository/livro.js
+++ infra/repository/livro.js
@@ -1,10 +1,15 @@
 const { query } = require("../database");
 
-function consultarTodos()  {
+async function consultarTodos() {
   const sql = `SELECT * FROM livro`;
-
-  const resultados = await query(sql)
-
+  const resultados = await query(sql);
   return resultados;
-
 }
+
+async function consultaTops() {
+  const sql = `SELECT * FROM livro WHERE top_livro = true`;
+  const resultados = await query(sql);
+  return resultados;
+}
+
+module.exports = { consultarTodos, consultaTops };
