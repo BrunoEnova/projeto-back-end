553f073d873526b6b618b6d29a27cfca6f6469ff
BrunoEnova <bruno.amaral13@enova.educacao.ba.gov.br>
2025-11-13 16:38:45 +0000

    ok

--- index.js
+++ index.js
@@ -3,8 +3,30 @@ const livroRepository = require("./infra/repository/livro");
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Oi sou túlio");
+app.get("/", async function (req, res) {
+  const livrosTops = await livroRepository.consultaTops();
+
+  let html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <ul>
+`;
+
+  for (livro of livrosTops) {
+    html += `<li>${livro}</li>`;
+  }
+
+  html += `
+    </ul>
+  </body>
+</html>`;
+
+  res.send(html);
 });
 
 const port = process.env.PORT || 3000;
